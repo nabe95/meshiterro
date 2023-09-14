71252fb33a48d1af75f1b9cebd5279c8fa2b394e
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-09-14 12:54:38 +0000

    [add] 部分テンプレート

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,7 +3,7 @@ class PostImage < ApplicationRecord
     belongs_to :user
     has_many :post_comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
-    
+
     def get_image
         if image.attached?
             image
@@ -19,7 +19,7 @@ class PostImage < ApplicationRecord
         end
         image
     end
-    
+
     def favorited_by?(user)
         favorites.exists?(user_id: user.id)
     end
